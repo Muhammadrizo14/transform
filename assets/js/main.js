88980c7efd206f8db0080314930df8eeaa44105c
Muhammadrizo <mtursunboev13@gmail.com>
2024-07-25 21:42:28 +0500

    add clients feedbacks

--- assets/js/main.js
+++ assets/js/main.js
@@ -666,6 +666,8 @@ numberInput.addEventListener("keydown", function (e) {
   ) {
     e.preventDefault();
   }
+  calculate();
+
 });
 
 function formatNumberRange(value) {
@@ -748,3 +750,28 @@ function calculateCommission(amount) {
     return amount * 0.015;
   }
 }
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    1000: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    1500: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+  }
+});
